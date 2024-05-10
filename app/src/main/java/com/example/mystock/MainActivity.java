eea0714b9ac2a73b5ae6b1bc54d3a15d1038f356
Rafael Sahyoun Rosa <83467259+rafaelsahyounrosa@users.noreply.github.com>
2024-05-10 11:45:57 -0300

    Vertical Scroll

--- app/src/main/java/com/example/mystock/MainActivity.java
+++ app/src/main/java/com/example/mystock/MainActivity.java
@@ -11,9 +11,6 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,12 +29,12 @@ public class MainActivity extends AppCompatActivity {
         checkBoxImportante = findViewById(R.id.checkBoxImportante);
         radioGroupCategoria = findViewById(R.id.radioGroupCategoria);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//
+//        });
     }
 
     public void limparDados (View view){
