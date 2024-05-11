f4e7db2cd7d252ad322664cd96f29046c6ff62e6
Rafael Sahyoun Rosa <83467259+rafaelsahyounrosa@users.noreply.github.com>
2024-05-11 13:23:04 -0300

    Cadastrar button logic and Spinner

--- app/src/main/java/com/example/mystock/MainActivity.java
+++ app/src/main/java/com/example/mystock/MainActivity.java
@@ -4,19 +4,25 @@ import static com.example.mystock.R.string.dados_limpos;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editTextNome, editTextQuantidade;
     private CheckBox checkBoxImportante;
     private RadioGroup radioGroupCategoria;
+    private Spinner spinnerCriticidade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,17 @@ public class MainActivity extends AppCompatActivity {
         editTextQuantidade = findViewById(R.id.editTextQuantidade);
         checkBoxImportante = findViewById(R.id.checkBoxImportante);
         radioGroupCategoria = findViewById(R.id.radioGroupCategoria);
+        spinnerCriticidade = findViewById(R.id.spinnerCriticidade);
+        spinnerCriticidade.setEnabled(false);
+
+        popularSpinner();
+
+        checkBoxImportante.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                spinnerCriticidade.setEnabled(isChecked);
+            }
+        });
 
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -35,6 +52,77 @@ public class MainActivity extends AppCompatActivity {
 //            return insets;
 //
 //        });
+
+
+    }
+
+//    public CompoundButton.OnCheckedChangeListener setSpinnerCriticidadeEnable(){
+//
+//        spinnerCriticidade.setEnabled(checkBoxImportante.isChecked());
+//        return null;
+//    }
+
+    private void popularSpinner(){
+        ArrayList<String> listaCriticidade = new ArrayList<>();
+
+        listaCriticidade.add(getString(R.string.baixa));
+        listaCriticidade.add(getString(R.string.media));
+        listaCriticidade.add(getString(R.string.alta));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                                                            android.R.layout.simple_list_item_1,
+                                                            listaCriticidade);
+
+        spinnerCriticidade.setAdapter(adapter);
+    }
+
+    public void validaDados(View view){
+
+        String nome = editTextNome.getText().toString();
+        String quantidade = editTextQuantidade.getText().toString();
+        Boolean importante = checkBoxImportante.isChecked();
+        String criticidade = spinnerCriticidade.getSelectedItem().toString();
+        int categoria = radioGroupCategoria.getCheckedRadioButtonId();
+
+        if(nome == null || nome.trim().isEmpty()){
+            Toast.makeText(this,
+                    getString(R.string.preciso_preencher_o_campo_nome_para_cadastrar),
+                    Toast.LENGTH_LONG).show();
+            editTextNome.requestFocus();
+            return;
+        } else if (quantidade == null || quantidade.trim().isEmpty()) {
+            Toast.makeText(this,
+                    getString(R.string.preciso_preecher_o_campo_de_quantidade_para_cadastrar),
+                    Toast.LENGTH_LONG).show();
+            editTextQuantidade.requestFocus();
+            return;
+        } else if (categoria < 0) {
+            Toast.makeText(this,
+                    getString(R.string.preciso_escolher_a_categoria_para_cadastrar),
+                    Toast.LENGTH_LONG).show();
+            radioGroupCategoria.requestFocus();
+            return;
+
+        } else if (importante) {
+            //Lógica pois o checkbox será opcional
+            if (criticidade == null || criticidade.trim().isEmpty()){
+                Toast.makeText(this,
+                        R.string.checkbox_importante_marcado_erro,
+                        Toast.LENGTH_LONG).show();
+                return;
+            }
+        }
+
+        Toast.makeText(this,
+                getString(R.string.dados_selecionados) +
+                        "\n" + getString(R.string.nomeLabel) + " " + nome +
+                        "\n" + getString(R.string.quantidadeLabel) + " " + quantidade +
+                        "\n" + getString(R.string.checkBoxImportante) + " " + importante +
+                        "\n" + getString(R.string.criticidadeLabel) + " " + criticidade +
+                        "\n" + getString(R.string.textViewCategoria) + " " + categoria
+                ,
+                Toast.LENGTH_LONG).show();
+
     }
 
     public void limparDados (View view){
@@ -42,6 +130,7 @@ public class MainActivity extends AppCompatActivity {
         editTextQuantidade.setText(null);
         checkBoxImportante.setChecked(false);
         radioGroupCategoria.clearCheck();
+        spinnerCriticidade.setSelection(0);
 
 
 
